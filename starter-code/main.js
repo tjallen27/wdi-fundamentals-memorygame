cadad199a81963112084f1ca411b3638dd2b89b3
Thomas Allen <thomasallen@Thomass-Air.home>
2017-01-13 10:29:50 +0000

    Updated Game Logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,21 +1,43 @@
 console.log("JS file is connected to HTML! Woo!")
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-cardOne == cardTwo
-cardThree == cardFour
+//var cardOne = "queen";
+//var cardTwo = "queen";
+//var cardThree = "king";
+//var cardFour = "king";
+//cardOne == cardTwo
+//cardThree == cardFour
+//
+//
+//if (cardOne === cardTwo) {
+//	alert('You found a match!');	
+//} else {
+//  alert('Sorry, try again.');
+//}
+//
+//if (cardThree === cardFour) {
+//	alert('You found a match!');	
+//} else {
+//  alert('Sorry, try again.');
+//}
 
 
-if (cardOne === cardTwo) {
-	alert('You found a match!');	
-} else {
-  alert('Sorry, try again.');
-}
+var createCards = function(){
+	//create gameboard variable
+	var gameBoard = document.getElementById('game-board');
 
-if (cardThree === cardFour) {
-	alert('You found a match!');	
-} else {
-  alert('Sorry, try again.');
-}
+	//create for loop that creates cards
+		for (i = 0; i<4; i++) {
+			
+			//create card element
+			var newCard = document.createElement('div');
+
+			//add class to card
+			newCard.className = "card";
+
+			//add card to board
+			gameBoard.appendChild(newCard);
+		}
+	}
+
+createCards();
+
