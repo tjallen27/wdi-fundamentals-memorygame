e8c325544c63861893dc16d32ac55daba8f6f9f1
Thomas Allen <thomasallen@Thomass-Air.home>
2017-01-13 09:09:33 +0000

    updated game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,18 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+cardOne == cardTwo
+cardThree == cardFour
+
+
+if (cardOne === cardTwo) {
+	alert('You found a match!');	
+} else {
+  alert('Sorry, try again.');
+}
+
+if (cardThree === cardFour) {
+	alert('You found a match!');	
+} else {
+  alert('Sorry, try again.');
+}
